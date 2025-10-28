b48dee547aa71cd2ff914d9166d508ab1e56a99e
Alexandre MONTIEL <alexandre.montiel@etu.imt-nord-europe.fr>
2025-10-28 23:33:40 +0100

    build : first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <main style="text-align: center; padding: 2rem">
+    <h1>Bienvenue</h1>
+  </main>
+</template>
+
+<script setup>
+</script>
+
+<style>
+body {
+  font-family: system-ui, sans-serif;
+  background-color: #fafafa;
+}
+</style>
